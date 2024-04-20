990f3228fde5e9b043099b51fc10219ef7bb3f89
philipuuu <topgun0415@naver.com>
2024-04-20 21:22:05 +0900

    JS Level 0 complete

---
+++ Programmers/LEVEL 0/문자열반복해서출력하기.js
@@ -0,0 +1,39 @@
+/**
+ * 문자열 str과 정수 n이 주어집니다.
+ * str이 n번 반복된 문자열을 만들어 출력하는 코드를 작성해 보세요.
+ *
+ * 제한사항
+ * 1 ≤ str의 길이 ≤ 10
+ * 1 ≤ n ≤ 5
+ *
+ * @format
+ */
+
+// let input = ['String', 5];
+
+// function repeatPrinter(input) {
+//   let str = input[0];
+//   let n = Number(input[1]);
+
+//   if (1 <= str.length >= 10 || 0 <= n >= 5) {
+//     console.log(str.repeat(n));
+//     return str.repeat(n);
+//   }
+// }
+
+// repeatPrinter(input);
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+  input = [line];
+}).on('close', function () {
+  str = input[0];
+  console.log(str);
+});
